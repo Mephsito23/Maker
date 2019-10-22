b3684ea8cbc92a4c6305a7343231b3e1be6761d7
Mephisto <xiezefu@outlook.com>
2019-10-22 13:47:22 +0800

    swift5 适配

--- Maker/Maker/Maker.swift
+++ Maker/Maker/Maker.swift
@@ -8,16 +8,14 @@
 
 import Foundation
 
-let temple = "IyBkaXNhYmxlZF9ydWxlczogIyBydWxlIGlkZW50aWZpZXJzIHRvIGV4Y2x1ZGUgZnJvbSBydW5uaW5nCiMgICAtIGNvbG9uCiMgICAtIGNvbW1hCiMgICAtIGNvbnRyb2xfc3RhdGVtZW50CiMgb3B0X2luX3J1bGVzOiAjIHNvbWUgcnVsZXMgYXJlIG9ubHkgb3B0LWluCiMgICAtIGVtcHR5X2NvdW50CiMgRmluZCBhbGwgdGhlIGF2YWlsYWJsZSBydWxlcyBieSBydW5uaW5nOgojIHN3aWZ0bGludCBydWxlcwojIGluY2x1ZGVkOiAjIHBhdGhzIHRvIGluY2x1ZGUgZHVyaW5nIGxpbnRpbmcuIGAtLXBhdGhgIGlzIGlnbm9yZWQgaWYgcHJlc2VudC4KIyAgIC0gR2FuawojIGV4Y2x1ZGVkOiAjIHBhdGhzIHRvIGlnbm9yZSBkdXJpbmcgbGludGluZy4gVGFrZXMgcHJlY2VkZW5jZSBvdmVyIGBpbmNsdWRlZGAuCiMgICAtIFBvZHMKIyAgIC0gR2Fuay9WZW5kb3JzCgojIGNvbmZpZ3VyYWJsZSBydWxlcyBjYW4gYmUgY3VzdG9taXplZCBmcm9tIHRoaXMgY29uZmlndXJhdGlvbiBmaWxlCiMgYmluYXJ5IHJ1bGVzIGNhbiBzZXQgdGhlaXIgc2V2ZXJpdHkgbGV2ZWwKIyBmb3JjZV9jYXN0OiB3YXJuaW5nICMgaW1wbGljaXRseQojIGZvcmNlX3RyeToKIyAgIHNldmVyaXR5OiB3YXJuaW5nICMgZXhwbGljaXRseQojIHJ1bGVzIHRoYXQgaGF2ZSBib3RoIHdhcm5pbmcgYW5kIGVycm9yIGxldmVscywgY2FuIHNldCBqdXN0IHRoZSB3YXJuaW5nIGxldmVsCiMgaW1wbGljaXRseQojIGxpbmVfbGVuZ3RoOiAxMjAKIyB0aGV5IGNhbiBzZXQgYm90aCBpbXBsaWNpdGx5IHdpdGggYW4gYXJyYXkKIyB0eXBlX2JvZHlfbGVuZ3RoOgojICAgLSAzMDAgIyB3YXJuaW5nCiMgICAtIDQwMCAjIGVycm9yCiMgb3IgdGhleSBjYW4gc2V0IGJvdGggZXhwbGljaXRseQojIGZpbGVfbGVuZ3RoOgojICAgd2FybmluZzogNTAwCiMgICBlcnJvcjogMTIwMDAKIyBjdXN0b21fcnVsZXM6CiMgICBwaXJhdGVzX2JlYXRfbmluamFzOiAjIHJ1bGUgaWRlbnRpZmllcgojICAgICBpbmNsdWRlZDogIi4qLnN3aWZ0IiAjIHJlZ2V4IHRoYXQgZGVmaW5lcyBwYXRocyB0byBpbmNsdWRlIGR1cmluZyBsaW50aW5nLiBvcHRpb25hbC4KIyAgICAgZXhjbHVkZWQ6ICIuKlRlc3Quc3dpZnQiICMgcmVnZXggdGhhdCBkZWZpbmVzIHBhdGhzIHRvIGV4Y2x1ZGUgZHVyaW5nIGxpbnRpbmcuIG9wdGlvbmFsCiMgICAgIG5hbWU6ICJQaXJhdGVzIEJlYXQgTmluamFzIiAjIHJ1bGUgbmFtZS4gb3B0aW9uYWwuCiMgICAgIHJlZ2V4OiAiKFtuLE5daW5qYSkiICMgbWF0Y2hpbmcgcGF0dGVybgojICAgICBtYXRjaF9raW5kczogIyBTeW50YXhLaW5kcyB0byBtYXRjaC4gb3B0aW9uYWwuCiMgICAgICAgLSBjb21tZW50CiMgICAgICAgLSBpZGVudGlmaWVyCiMgICAgIG1lc3NhZ2U6ICJQaXJhdGVzIGFyZSBiZXR0ZXIgdGhhbiBuaW5qYXMuIiAjIHZpb2xhdGlvbiBtZXNzYWdlLiBvcHRpb25hbC4KIyAgICAgc2V2ZXJpdHk6IGVycm9yICMgdmlvbGF0aW9uIHNldmVyaXR5LiBvcHRpb25hbC4KIyAgIG5vX2hpZGluZ19pbl9zdHJpbmdzOgojICAgICByZWdleDogIihyeF9kaXNwb3NlQmFnKSIKIyAgICAgbWF0Y2hfa2luZHM6IGtleXdvcmQKIyBuYW1pbmcgcnVsZXMgY2FuIHNldCB3YXJuaW5ncy9lcnJvcnMgZm9yIG1pbl9sZW5ndGggYW5kIG1heF9sZW5ndGgKIyBhZGRpdGlvbmFsbHkgdGhleSBjYW4gc2V0IGV4Y2x1ZGVkIG5hbWVzCiMgdHlwZV9uYW1lOgojICAgbWluX2xlbmd0aDogMiAjIG9ubHkgd2FybmluZwojICAgbWF4X2xlbmd0aDogIyB3YXJuaW5nIGFuZCBlcnJvcgojICAgICB3YXJuaW5nOiA0MAojICAgICBlcnJvcjogMTAwMAojICAgZXhjbHVkZWQ6IGlQaG9uZSAjIGV4Y2x1ZGVkIHZpYSBzdHJpbmcKIyBpZGVudGlmaWVyX25hbWU6CiMgICBtaW5fbGVuZ3RoOiAjIG9ubHkgbWluX2xlbmd0aAojICAgICBlcnJvcjogMSAjIG9ubHkgZXJyb3IKIyAgIGV4Y2x1ZGVkOiAjIGV4Y2x1ZGVkIHZpYSBzdHJpbmcgYXJyYXkKIyAgICAgLSBpZAojICAgICAtIFVSTAojICAgICAtIEdsb2JhbEFQSUtleQojIHJlcG9ydGVyOiAieGNvZGUiICMgcmVwb3J0ZXIgdHlwZSAoeGNvZGUsIGpzb24sIGNzdiwgY2hlY2tzdHlsZSwganVuaXQsIGh0bWwsIGVtb2ppKQo="
+let temple = "ZGlzYWJsZWRfcnVsZXM6ICMg5omn6KGM5pe25o6S6Zmk5o6J55qE6KeE5YiZCiAgLSBjb2xvbgogIC0gY29tbWEKICAtIGNvbnRyb2xfc3RhdGVtZW50Cm9wdF9pbl9ydWxlczogIyDkuIDkupvop4TliJnku4Xku4XmmK/lj6/pgInnmoQKICAtIGVtcHR5X2NvdW50Cm1pc3NpbmdfZG9jczoKICAtIGludGVybmFsCiAgIyDlj6/ku6XpgJrov4fmiafooYzlpoLkuIvmjIfku6TmnaXmn6Xmib7miYDmnInlj6/nlKjnmoTop4TliJk6CiAgIyBzd2lmdGxpbnQgcnVsZXMKaW5jbHVkZWQ6ICMg5omn6KGMIGxpbnRpbmcg5pe25YyF5ZCr55qE6Lev5b6E44CC5aaC5p6c5Ye6546w6L+Z5LiqIGAtLXBhdGhgIOS8muiiq+W/veeVpeOAggogIC0gYW5pbWF0aW9uCmV4Y2x1ZGVkOiAjIOaJp+ihjCBsaW50aW5nIOaXtuW/veeVpeeahOi3r+W+hOOAgiDkvJjlhYjnuqfmr5QgYGluY2x1ZGVkYCDmm7Tpq5jjgIIKICAtIENhcnRoYWdlCiAgLSBQb2RzCgojIOWPr+mFjee9rueahOinhOWImeWPr+S7pemAmui/h+i/meS4qumFjee9ruaWh+S7tuadpeiHquWumuS5iQojIOS6jOi/m+WItuinhOWImeWPr+S7peiuvue9ruS7luS7rOeahOS4peagvOeoi+W6pgpmb3JjZV9jYXN0OiB3YXJuaW5nICMg6ZqQ5byPCmZvcmNlX3RyeToKICBzZXZlcml0eTogd2FybmluZyAjIOaYvuW8jwojIOWQjOaXtuacieitpuWRiuWSjOmUmeivr+etiee6p+eahOinhOWIme+8jOWPr+S7peWPquiuvue9ruWug+eahOitpuWRiuetiee6pwojIOmakOW8jwpsaW5lX2xlbmd0aDogMTIwCiMg5Y+v5Lul6YCa6L+H5LiA5Liq5pWw57uE5ZCM5pe26L+b6KGM6ZqQ5byP6K6+572uCnR5cGVfYm9keV9sZW5ndGg6CiAgLSAzMDAgIyB3YXJuaW5nCiAgLSA0MDAgIyBlcnJvcgojIOaIluiAheS5n+WPr+S7peWQjOaXtui/m+ihjOaYvuW8j+iuvue9rgpmaWxlX2xlbmd0aDoKICB3YXJuaW5nOiA1MDAKICBlcnJvcjogMTIwMAojIOWRveWQjeinhOWImeWPr+S7peiuvue9ruacgOWwj+mVv+W6puWSjOacgOWkp+eoi+W6pueahOitpuWRii/plJnor68KIyDmraTlpJblroPku6zkuZ/lj6/ku6Xorr7nva7mjpLpmaTlnKjlpJbnmoTlkI3lrZcKdHlwZV9uYW1lOgogIG1pbl9sZW5ndGg6IDQgIyDlj6rmmK/orablkYoKICBtYXhfbGVuZ3RoOiAjIOitpuWRiuWSjOmUmeivrwogICAgd2FybmluZzogNDAKICAgIGVycm9yOiA1MAogIGV4Y2x1ZGVkOiBpUGhvbmUgIyDmjpLpmaTmn5DkuKrlkI3lrZcKaWRlbnRpZmllcl9uYW1lOgogIG1pbl9sZW5ndGg6ICMg5Y+q5pyJ5pyA5bCP6ZW/5bqmCiAgICBlcnJvcjogNCAjIOWPquaciemUmeivrwogIGV4Y2x1ZGVkOiAjIOaOkumZpOafkOS6m+WQjeWtlwogICAgLSBpZAogICAgLSBVUkwKICAgIC0gR2xvYmFsQVBJS2V5CnJlcG9ydGVyOiAieGNvZGUiICMg5oql5ZGK57G75Z6LICh4Y29kZSwganNvbiwgY3N2LCBjaGVja3N0eWxlLCBqdW5pdCwgaHRtbCwgZW1vamkpCg=="
 
 enum OutputType {
-
     case error
     case standard
 }
 
 enum ParameterType: String {
-
     case swiftlintyml = "y"
     case podfile = "p"
     case unkown
@@ -26,24 +24,20 @@ enum ParameterType: String {
         switch raw {
         case "y":
             self = .swiftlintyml
-            break
         case "p":
             self = .podfile
         default:
             self = .unkown
-            break
         }
     }
 }
 
 final class ConsoleIO {
-
     func fetchOption(value: String) -> (option: ParameterType, value: String) {
         return (ParameterType(raw: value), value)
     }
 
     func logUsage() {
-
         let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
         print("Maker Usage:")
         print("🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡 🤡")
@@ -51,11 +45,9 @@ final class ConsoleIO {
         print("\(executableName) -p: autocreate a Podfile file with template")
         print("❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️")
         print("Enjoy your coding time!")
-
     }
 
     func write(message: String, to: OutputType = .standard) {
-
         switch to {
         case .standard:
             print("\u{001B}[;m\(message)")
@@ -66,13 +58,11 @@ final class ConsoleIO {
 }
 
 final class Maker {
-
     let consoleIO = ConsoleIO()
 
     // MARK: - Public Method
 
     func staticMode() {
-
         let argCount = CommandLine.argc
         let argument = CommandLine.arguments[1]
 
@@ -81,14 +71,13 @@ final class Maker {
             return
         }
 
-        let (param, _) = consoleIO.fetchOption(value: argument.substring(from: argument.characters.index(argument.startIndex, offsetBy: 1)))
+        let command = String(argument.suffix(1))
+        let (param, _) = consoleIO.fetchOption(value: command)
         switch param {
         case .podfile:
             createPodFile()
-            break
         case .swiftlintyml:
             createYML()
-            break
         default:
             consoleIO.write(message: "Sorry! you input unrecognized params")
         }
@@ -96,16 +85,14 @@ final class Maker {
 }
 
 extension Maker {
-
     // MARK: - Business Logic
 
     fileprivate func createYML() {
-
         let currentPath = FileManager.default.currentDirectoryPath
 
         if FileManager.default.createFile(atPath: "\(currentPath)/.swiftlint.yml", contents: Data(base64Encoded: temple), attributes: nil) {
             consoleIO.write(message: "create .swiftlint.yml success.", to: .standard)
-        }else {
+        } else {
             consoleIO.write(message: "create .swiftlint.yml fail.", to: .error)
         }
     }
